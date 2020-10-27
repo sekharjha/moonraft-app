b6f313567de9ef1fc5fa3407c233f1165342963d
Sekhar Jha <sekharjha14@gmail.com>
2020-10-27 13:36:46 +0530

    memory issue

--- src/core/View.js
+++ src/core/View.js
@@ -1,6 +1,5 @@
 
 import React, { Component } from 'react'
-import Input from "../auth/helper/input"
 import ViewPage from "../component/View/view"
 import Base from './Base'
 class View extends Component{
